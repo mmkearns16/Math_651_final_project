5040b59c4dc634bd6333debbeff68a226f0dc696
Max Kearns <mmkearns16@gmail.com>
2018-12-15 10:34:21 -0500

    draft

--- ForecastPred.R
+++ ForecastPred.R
@@ -31,29 +31,6 @@ forecast.lm = cbind(log(base.2016$count),lower.pred,upper.pred)
 ininterval.lm = cbind(forecast.lm[,2]<=forecast.lm[,1] & forecast.lm[,3]>=forecast.lm[,1])
 length(which(ininterval.lm))
 
-#Prediction Intervals: NB
-#NB
-fitted.nb = exp(-10.17)*exp(0.402*base.2016$log_gdp_per_cap)*exp(0.498*base.2016$log_pop)*exp(0.693*base.2016$host)*exp(1.03*base.2016$comm_soviet) 
-attach(base.total)
-nb.df = data.frame(count,log_pop = log(pop),log_gdp_per_cap = log(gdp_per_cap),host,comm_soviet)
-olympic.nb<- glm.nb(count ~ log_pop + log_gdp_per_cap + host + comm_soviet,data = nb.df)
-mse.nb = 386.80/382
-
-spred.nb = c()
-for (i in 1:nrow(base.2016)){
-  xh = as.matrix(cbind(1,nb.df$log_gdp_per_cap[i],nb.df$log_pop[i],nb.df$host[i],nb.df$comm_soviet[i]))
-  s2 = mse.nb*(1+xh%*%solve(t(X)%*%X)%*%t(xh))
-  spred.nb = c(spred.nb,sqrt(s2))
-}
-
-lower.pred.nb = fitted.nb - qt(1-0.05/2,76)*spred.nb
-upper.pred.nb = fitted.nb + qt(1-0.05/2,76)*spred.nb
-
-forecast.nb = cbind(base.2016$count,lower.pred.nb,upper.pred.nb)
-
-ininterval = cbind(forecast.nb[,2]<=forecast.nb[,1] & forecast.nb[,3]>=forecast.nb[,1])
-length(which(ininterval))
-
 #NB Method 2 (works!)
 library(tidyverse)
 library(ciTools)
@@ -63,15 +40,37 @@ set.seed(20181215)
 
 base.2016 = base[which(base$year==2016),]
 attach(base.2016)
-base.2016 = data.frame(country,count,year,log_pop=log(pop),log_gdp_pcap = log(gdp/pop),host,comm_soviet)
+base.2016 = data.frame(country,count,year,log_pop=log(pop),log_gdp_per_cap = log(gdp/pop),host,comm_soviet)
 
 newData <- data.frame(base.2016[,c(2,4,5,6,7)])
 train_data <- Olympic_v2[,3:7]
 
 #Generate prediction intervals
-olympic.nb = glm.nb(count ~ log_pop + log_gdp_pcap + host + comm_soviet,data=train_data)
+olympic.nb = glm.nb(count ~ log_pop + log_gdp_per_cap + host + comm_soviet,data=train_data)
 
 #add_pi comes from the library ciTools
 olympic.nb_pint <- add_pi(tb=newData,fit=olympic.nb, names=c("lpb", "upb"), alpha=0.1, nSims=20000)
+
+
+olympic.nb_pint %>%
+  mutate(inside=(pred >lpb & pred<upb))-> preds.nb
+
+summary(preds.nb)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
